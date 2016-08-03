5a08a3fe41ebee94b353c881ca03a5dc3e048066
madanadit <adit@alluxio.com>
2016-08-02 19:32:26 -0700

    minor change

--- get-swift-config.sh
+++ get-swift-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pushd .
-cd ceph-ansible
+#pushd .
+#cd ceph-ansible
 vagrant ssh mon0 -c "sudo radosgw-admin user create --uid=ceph-swift --display-name='Ceph Swift'"
 vagrant ssh mon0 -c "sudo radosgw-admin subuser create --uid=ceph-swift --subuser=ceph-swift:ceph-swift --access=full"
 KEY=`vagrant ssh mon0 -c "sudo radosgw-admin key create --subuser=ceph-swift:ceph-swift --key-type=swift --gen-secret" | grep "ceph-swift:ceph-swift" -A 1 | grep "secret_key"`
@@ -9,7 +9,7 @@ KEY=${KEY##*:}
 
 RGW0=`cat .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory | grep mon0 -m 1`
 MON_HOST=`cut -d "=" -f 2 <<< $RGW0 | cut -d " " -f 1`
-popd
+#popd
 
 SWIFT_AUTH=http://${MON_HOST}:8080/auth
 SWIFT_USER=ceph-swift:ceph-swift
